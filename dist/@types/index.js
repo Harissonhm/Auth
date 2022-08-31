655aa400c7ff8ddb4556ac4ac4bfdaf4c80d6487
Harissonhm <harisson.hm@gmail.com>
2022-08-31 15:09:13 -0300

    Add_req_res_next

--- dist/@types/index.js
+++ dist/@types/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('executei');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/status', (req, res, next) => {
+    res.status(200).send({ foo: 'sucesso total! se ta doido' });
+});
+app.listen(3000, () => {
+    console.log('Aplicação executando na porta 3000!');
+});
--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,1 +1,11 @@
-console.log('executei');
+import express, {Request, Response, NextFunction} from 'express';
+
+const app = express();
+
+app.get('/status',(req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({foo: 'sucesso total! se ta doido'});
+});
+    app.listen(3000, () =>{
+    console.log('Aplicação executando na porta 3000!'); 
+});
+
